62339567b909c1c5b3ca3911a23b877cb5ffb23a
Ken Wang <ykw@pitt.edu>
2022-04-03 09:17:24 -0400

    added bootstrap

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, redirect, url_for, render_template
+from flask_bootstrap import Bootstrap
 import sqlite3 as sql
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/<int:score>")
 def hello_world(score):
